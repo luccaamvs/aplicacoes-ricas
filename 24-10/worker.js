a2af7c14b095f6fc40cdd6cbdb175676cbe3e334
LuccaBordin <lbordin.design@gmail.com>
2022-10-31 21:55:44 -0300

    Subindo primeira versão da ADO de Canvas

---
+++ 24-10/worker.js
@@ -0,0 +1,10 @@
+let mensagem = "inicio";
+
+var j = 0; 
+var n = 50000;
+
+for (var i=0; i<n;i++){
+    mensagem = mensagem + "<br/>" +i;
+}
+
+mensagem = mensagem + "<br/>" + 
---
+++ adoCanvas/script.js
@@ -0,0 +1,12 @@
+// Primeira coisa a fazer é pegar o Canvas e o configurar.
+
+window.addEventListener("load", () => {
+    const canvas = document.querySelector('#meuCanvas');
+
+//Definir context e o definir como 2D:
+
+    const ctx = canvas.getContext('2d');
+
+ctx.fillRect
+});
+
